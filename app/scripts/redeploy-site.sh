915aac61e77e186d4f017f8bf808aa54dc898e96
Logan <logan.hoogendijk@gmail.com>
2022-06-23 20:41:18 -0400

    changed redeploy-site.sh

--- app/scripts/redeploy-site.sh
+++ app/scripts/redeploy-site.sh
@@ -2,9 +2,9 @@
 # Author: Logan Hoogendijk
 # Script follows below:
 
-tmux kill-server
 cd ~/project-bored-apes
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-tmux new-session -d -s portfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl daemon-reload
+systemctl restart myportfolio
